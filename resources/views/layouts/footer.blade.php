b7cfbaa918907f42a77f9e5daed9f653fd42d88a
Victor Morakinyo <morakinyovictor1@gmail.com>
2025-08-06 11:18:19 +0100

    fix favicon icon and dashboard footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="content py-3">
       <div class="row fs-sm">
         <div class="col-sm-6 order-sm-2 py-1 text-center text-sm-end">
-          Crafted with <i class="fa fa-heart text-danger"></i> by <a class="fw-semibold" href="https://pixelcave.com" target="_blank">Dominahl Technologies</a>
+          Crafted with <i class="fa fa-heart text-danger"></i> by <a class="fw-semibold" href="https://pixelcave.com" target="_blank">Freebyz Technology Ltd</a>
         </div>
         <div class="col-sm-6 order-sm-1 py-1 text-center text-sm-start">
           <a class="fw-semibold" href="" target="_blank">Famlic App</a> &copy; <span data-toggle="year-copy"></span>
